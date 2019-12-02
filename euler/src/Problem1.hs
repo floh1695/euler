6c3d36a6e7a95ff3d0a356befbfc9870e81dff50
Charles Bayley <cbayley@gmail.com>
2019-12-02 02:14:36 +0000

    Cleaned up

--- euler/src/Problem1.hs
+++ euler/src/Problem1.hs
@@ -16,6 +16,6 @@ problem1 :: Integer
 problem1 = solution
   where
     baseNumbers     = [1..999]
-    predicate n     = n `divisibleBy` 3 || n `divisibleBy` 5
+    predicate n     = or $ [divisibleBy n] <*> [3, 5]
     selectedNumbers = filter predicate baseNumbers
     solution        = sum selectedNumbers
