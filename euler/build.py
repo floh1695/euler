f0d13c2757e15b5f4b6818ac6e5ba75d4627eaea
Charles Bayley <cbayley@gmail.com>
2019-12-08 04:18:41 +0000

    Large number of changes (should have been multiple commits)

--- euler/build.py
+++ euler/build.py
@@ -2,8 +2,24 @@
 
 import subprocess
 
-def any(iterr):
-  return len(iterr) > 0
+def equal_c(value):
+  return lambda other: value == other
+
+def any(l, f=None):
+  if f == None:
+    return len(l) > 0
+  else:
+    passed = list(filter(f, l))
+    return any(passed)
+
+def handle_pipe(pipe, f=None):
+  out = pipe.read().decode('utf-8')
+
+  if any(out):
+    print(out)
+
+  if f != None:
+    f.write(out)
 
 def process(args, out_f=None):
   p = subprocess.Popen(
@@ -13,27 +29,32 @@ def process(args, out_f=None):
 
   p.wait()
 
-  stdout = p.stdout \
-    .read() \
-    .decode('utf-8')
+  handle_pipe(p.stdout, out_f)
+  handle_pipe(p.stderr)
 
-  stderr = p.stderr \
-    .read() \
-    .decode('utf-8')
+class JoinFile():
+  def __init__(self, files):
+    self._files = files
 
-  if any(stdout):
-    print(stdout)
+  def write(self, data):
+    for f in self._files:
+      f.write(data)
 
-  if any(stderr):
-    print(stderr)
-
-  if out_f != None:
-    out_f.write(stdout)
-
-def build():
+def build(commit=False):
   process(['stack', 'build'])
-  with open('.report.secret.md', 'w+') as results:
-    process(['stack', 'run', 'euler-exe'], out_f=results)
+  with open('.report.secret.md', 'w+') as report, \
+      open('.solutions.secret.md', 'w+') as solutions:
+    to_join = [report]
+    if commit:
+      to_join.append(solutions)
+
+    joined = JoinFile(to_join)
+
+    process(['stack', 'run', 'euler-exe'], joined)
 
 if __name__ == '__main__':
-  build()
+  import sys
+  args = sys.argv[1:]
+
+  commit = any(args, equal_c('--commit'))
+  build(commit=commit)
